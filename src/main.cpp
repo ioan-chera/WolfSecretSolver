b8f9a409ef9f2705067ae774f85247a4ff30f8ea
Ioan Chera <ioan.chera@gmail.com>
2018-12-09 16:35:56 +0200

    Init project

---
+++ src/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  WolfSecretSolver
+//
+//  Created by ioan on 09.12.2018.
+//  Copyright © 2018 Ioan Chera. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
